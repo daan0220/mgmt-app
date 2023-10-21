68dee50756070269e61f145395274ada19806759
daikiando <daikiando@andoutaikinoMacBook-Pro.local>
2023-10-21 18:38:14 +0900

    first commit

---
+++ server/sampleData.js
@@ -0,0 +1,78 @@
+const projects = [
+    {
+      id: '1',
+      clientId: '1',
+      name: 'eCommerce Website',
+      description:
+        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.',
+      status: 'In Progress',
+    },
+    {
+      id: '2',
+      clientId: '2',
+      name: 'Dating App',
+      description:
+        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.',
+      status: 'In Progress',
+    },
+    {
+      id: '3',
+      clientId: '3',
+      name: 'SEO Project',
+      description:
+        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.',
+      status: 'In Progress',
+    },
+    {
+      id: '4',
+      clientId: '4',
+      name: 'Design Prototype',
+      description:
+        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.',
+      status: 'Done',
+    },
+    {
+      id: '5',
+      clientId: '5',
+      name: 'Auction Website',
+      description:
+        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.',
+      status: 'In Progress',
+    },
+  ];
+  
+  // Clients
+  const clients = [
+    {
+      id: '1',
+      name: 'リオネル・メッシ',
+      email: 'messi@gmail.com',
+      phone: '080-5672-4333',
+    },
+    {
+      id: '2',
+      name: 'ネイマール',
+      email: 'neymar@gmail.com',
+      phone: '080-5678-3322',
+    },
+    {
+      id: '3',
+      name: '安藤太紀',
+      email: 'daiki@gmail.com',
+      phone: '080-3318-4333',
+    },
+    {
+      id: '4',
+      name: '安斉星来',
+      email: 'seira@gmail.com',
+      phone: '090-562-6787',
+    },
+    {
+      id: '5',
+      name: 'ルイス・スアレス',
+      email: 'ruis@gmail.com',
+      phone: '070-468-8887',
+    },
+  ];
+  
+  module.exports = { projects, clients };
